203d15d26d75b5c2700f31530bef864e61dfb840
rendy andika <rendyndika@gmail.com>
2024-10-09 12:41:14 +0700

    feat: add features section

--- app/_components/features.tsx
+++ app/_components/features.tsx
@@ -1,7 +1,108 @@
-import React from 'react'
+import { Button } from "@nextui-org/button";
+import Image from "next/image";
+
+import styles from "@/styles/style.module.scss";
+import ImgFeaturesPayment from "@/assets/illustrations/features-payment.svg";
+import ImgFeaturesInvoice from "@/assets/illustrations/features-invoice.svg";
 
 export default function Features() {
   return (
-    <div>Features</div>
+    <div className={styles.features}>
+      <div className={styles.features__background} />
+      <div className={styles.features__container}>
+        <div className={styles.features__head}>
+          <h2 className={styles.text_heading_1}>Features</h2>
+          <h3 className={`${styles.text_super} ${styles.features__title}`}>What is <span className={styles.text_gradient_primary}>Zanbio</span></h3>
+          <h4
+            className={`${styles.text_subtitle} ${styles.features__subtitle}`}
+          >
+            Zanbio helps you streamline your finances by automating Accounts
+            Receivable, Accounts Payable, and invoice generation. With
+            everything in one place, managing your business&apos;s financial
+            operations becomes effortless and efficient.{" "}
+          </h4>
+        </div>
+
+        <div className={styles.features__content}>
+          <div className={styles.features__content_container}>
+            <h4
+              className={`${styles.text_heading_1}  ${styles.features__content_head}`}
+            >
+              High Level Product
+            </h4>
+            <h5
+              className={`${styles.text_super} ${styles.features__content_title}`}
+            >
+              Account{" "}
+              <span className={styles.text_gradient_primary}>Receivable</span>
+            </h5>
+            <h6
+              className={`${styles.text_subtitle} ${styles.features__content_subtitle}`}
+            >
+              Take control of your outgoing payments with ease. Zanbio’s
+              Accounts Payable feature tracks all your bills and expenses,
+              ensuring you never miss a payment deadline. Stay on top of your
+              obligations and manage your cash flow efficiently with automated
+              reminders and real-time updates.
+            </h6>
+
+            <Button className={styles.features__content_cta} variant="bordered">
+              Learn more
+            </Button>
+          </div>
+          <div className={styles.features__content_img}>
+            <Image
+              priority
+              alt="features-payment"
+              className="size-full"
+              height={100}
+              src={ImgFeaturesPayment}
+              width={100}
+            />
+          </div>
+        </div>
+
+        <div
+          className={`${styles.features__content} ${styles.features__content_swap}`}
+        >
+          <div className={styles.features__content_container}>
+            <h4
+              className={`${styles.text_heading_1}  ${styles.features__content_head}`}
+            >
+              High Level Product
+            </h4>
+            <h5
+              className={`${styles.text_super} ${styles.features__content_title}`}
+            >
+              Account{" "}
+              <span className={styles.text_gradient_primary}>Payable</span>
+            </h5>
+            <h6
+              className={`${styles.text_subtitle} ${styles.features__content_subtitle}`}
+            >
+              Get paid faster with Zanbio’s streamlined Accounts Receivable
+              system. Automatically generate invoices and track customer
+              payments in one simple dashboard. Stay informed on which clients
+              owe you, follow up with reminders, and watch your revenue flow in
+              without hassle.
+            </h6>
+
+            <Button className={styles.features__content_cta} variant="bordered">
+              Learn more
+            </Button>
+          </div>
+          <div className={styles.features__content_img}>
+            <Image
+              priority
+              alt="features-payment"
+              className="size-full"
+              height={100}
+              src={ImgFeaturesInvoice}
+              width={100}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import {
   NavbarBrand,
@@ -9,50 +9,48 @@ import {
   NavbarMenuToggle,
   Navbar as NextUINavbar,
 } from "@nextui-org/navbar";
-import styles from '@/styles/style.module.scss'
 import { Button } from "@nextui-org/button";
 import { Link } from "@nextui-org/link";
 import { Tab, Tabs } from "@nextui-org/tabs";
 import NextLink from "next/link";
 
-import {
-  Logo
-} from "@/components/icons";
+import styles from "@/styles/style.module.scss";
+import { Logo } from "@/components/icons";
 import { siteConfig } from "@/config/site";
 
 export const Navbar = () => {
   return (
-    <NextUINavbar maxWidth="xl" position="sticky" className="border-b py-2">
-      <NavbarContent  justify="start">
+    <NextUINavbar className="border-b py-2" maxWidth="xl" position="sticky">
+      <NavbarContent justify="start">
         <NavbarBrand as="li">
           <NextLink href="/">
-            <Logo className="size-36 lg:size-52"/>
+            <Logo className="size-36 lg:size-52" />
           </NextLink>
-        </NavbarBrand>    
+        </NavbarBrand>
       </NavbarContent>
 
-      <NavbarContent
-        className="hidden sm:flex"
-        justify="center"
-      >
+      <NavbarContent className="hidden sm:flex" justify="center">
         <NavbarItem className="hidden md:flex">
-          <Tabs key={'tabs-menu'} size={'lg'} aria-label="Tabs sizes" className="gap-0">
+          <Tabs
+            key={"tabs-menu"}
+            aria-label="Tabs sizes"
+            className="gap-0"
+            size={"lg"}
+          >
             {siteConfig.navItems.map((item) => (
-              <Tab key={item.href} title={item.label} className="px-10"/>
+              <Tab key={item.href} className="px-10" title={item.label} />
             ))}
           </Tabs>
         </NavbarItem>
       </NavbarContent>
 
-      <NavbarContent
-        className="hidden sm:flex"
-        justify="end"
-      >
+      <NavbarContent className="hidden sm:flex" justify="end">
         <NavbarItem className="hidden md:flex">
           <Button
             isExternal
             as={Link}
             className={`${styles.button_primary} h-[46px]`}
+            style={{ fontSize: "14px" }}
             variant="flat"
           >
             Talk to Sales
@@ -68,11 +66,7 @@ export const Navbar = () => {
         <div className="mt-2 flex flex-col gap-4">
           {siteConfig.navMenuItems.map((item, index) => (
             <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                color="foreground"
-                href="#"
-                size="md"
-              >
+              <Link color="foreground" href="#" size="md">
                 {item.label}
               </Link>
             </NavbarMenuItem>
