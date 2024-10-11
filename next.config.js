6f864457d576f9e01cbe52285f1aed626bd6d35c
rendy andika <rendyndika@gmail.com>
2024-10-11 14:40:09 +0700

    temporary: disabled ts on build

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
